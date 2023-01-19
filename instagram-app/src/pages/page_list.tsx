679847012bd5f4e490e53f7b850f79bc39f1d738
dongeun-i <dongeun1014@naver.com>
2023-01-19 23:29:10 +0900

    add home

--- instagram-app/src/pages/page_list.tsx
+++ instagram-app/src/pages/page_list.tsx
@@ -1,11 +1,9 @@
-import React, { useState, useEffect, Suspense, lazy } from "react";
+import React, { lazy, useEffect } from "react";
 import { useNavigate } from "react-router";
 import { Routes, Route } from "react-router-dom";
-// import { useSelector } from "react-redux";
-//
-//module
 
 const LoginPage = lazy(() => import("./login.page"));
+const HomePage = lazy(() => import("./home.page"));
 
 type pageListType = {
     isLogin: boolean;
@@ -18,16 +16,12 @@ export const PageList = (props: pageListType) => {
             navigate("/login", { replace: true });
         }
     }, [props.isLogin]);
+
     return (
-        <Suspense fallback={<div>로딩중...</div>}>
-            <Routes>
-                {/* <Route  path="/home" element={HomeMainPage} /> */}
-                <Route path="/login" element={<LoginPage />} />
-                {/* <Route  path="/settings" element={SettingPage} /> */}
-                {/* <Route  path="/admin" element={AdminMainPage} /> */}
-                {/* <Route  path="/order" element={OrderMainListPage} /> */}
-                {/* <Route  path="/available/list" element={AvailableListPage} /> */}
-            </Routes>
-        </Suspense>
+        <Routes>
+            <Route path="/" element={<HomePage />}></Route>
+            <Route path="/login" element={<LoginPage />}></Route>
+            {/* <Route path="/sign" element={<SignPage />}></Route> */}
+        </Routes>
     );
 };
