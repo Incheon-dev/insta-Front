26f6f8c3124a04272830a9c60ac317a90b50e8ad
dongeun-l <dongeun1014@naver.com>
2023-01-30 17:20:49 +0900

    messageModal 작업

--- instagram-app/src/components/modal/messageMoal.tsx
+++ instagram-app/src/components/modal/messageMoal.tsx
@@ -1,110 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+// import { reducerState } from "../../store";
+import { useNavigate } from "react-router";
 import * as S from "../../styled";
 
-let onError = false;
-let onWarning = false;
-let onConfirm = false;
-let onSuccess = false;
-let onInfo = false;
+type ModalButton = {
+    text: string;
+    onClick: (value?: any) => void;
+    style?: React.CSSProperties;
+};
 
 interface ModalProps {
     title?: string;
     message?: string;
-    onOk?: (value: any) => void;
-    onCancel?: () => void;
+    ok?: ModalButton;
+    cancel?: ModalButton;
     width?: number;
     style?: any;
+    visible?: boolean;
 }
 
 export const Modal = ({ ...props }: ModalProps) => {
+    const [isActive, setisActive] = useState<boolean>(true);
+    // const modalSelector = useSelector((state: reducerState) => state.modal);
+    // useEffect(() => {
+    //     console.log("modalSelector", modalSelector);
+    // }, [modalSelector]);
+    useEffect(() => {
+        if (props.visible != undefined) {
+            setisActive(props.visible);
+        }
+    }, [props.visible]);
+
     return (
-        <S.ModalBackground>
+        <S.ModalBackground $visible={isActive}>
             <S.ModalContainer>
                 <S.ModalHeader>
                     <S.ModalTitle>{props.title}</S.ModalTitle>
                 </S.ModalHeader>
                 <S.ModalMessage>{props.message}</S.ModalMessage>
+                <S.ModalBtnGroup>
+                    {props.ok && (
+                        <S.ModalOkBtn
+                            onClick={(e) => {
+                                props.ok?.onClick(e);
+                            }}
+                            style={{ ...props.ok.style }}
+                        >
+                            {props.ok.text}
+                        </S.ModalOkBtn>
+                    )}
+                    {props.cancel && (
+                        <S.ModalCancelBtn
+                            style={{ ...props.cancel.style }}
+                            onClick={(e) => {
+                                props.cancel?.onClick(e);
+                            }}
+                        >
+                            {props.cancel.text}
+                        </S.ModalCancelBtn>
+                    )}
+                </S.ModalBtnGroup>
             </S.ModalContainer>
         </S.ModalBackground>
     );
 };
-
-// export const ModalConfirm = (
-//     message: string,
-//     onExit: (result: boolean) => void,
-//     width?: string | number
-// ) => {
-//     if (!onConfirm) {
-//         Modal.confirm({
-//             title: "확인",
-//             content: message,
-//             cancelText: "취소",
-//             okText: "확인",
-//             onOk: () => {
-//                 onConfirm = false;
-//                 onExit(true);
-//             },
-//             onCancel: () => {
-//                 onConfirm = false;
-//                 onExit(false);
-//             },
-//             width: width,
-//         });
-//         onConfirm = true;
-//     }
-// };
-
-// export const ModalSuccess = (message: string) => {
-//     if (!onSuccess) {
-//         Modal.success({
-//             content: message,
-//             okText: "확인",
-//             onOk: () => {
-//                 onSuccess = false;
-//             },
-//         });
-//         onSuccess = true;
-//     }
-// };
-
-// export const ModalInfo = (message: string) => {
-//     if (!onInfo) {
-//         Modal.info({
-//             title: "안내",
-//             content: message,
-//             okText: "확인",
-//             onOk: () => {
-//                 onInfo = false;
-//             },
-//         });
-//         onInfo = true;
-//     }
-// };
-
-// export const ModalError = (message: string) => {
-//     if (!onError) {
-//         Modal.error({
-//             title: "에러",
-//             content: message,
-//             okText: "확인",
-//             onOk: () => {
-//                 onError = false;
-//             },
-//         });
-//         onError = true;
-//     }
-// };
-
-// export const ModalWarning = (message: string) => {
-//     if (!onWarning) {
-//         Modal.warning({
-//             title: "경고",
-//             content: message,
-//             okText: "확인",
-//             onOk: () => {
-//                 onWarning = false;
-//             },
-//         });
-//         onWarning = true;
-//     }
-// };
