1f802cc011a8ecd189092d9d01fa8ad23b104f8a
dongeun-l <dongeun1014@naver.com>
2023-01-30 17:20:27 +0900

    state 상태 유지하기

--- instagram-app/src/index.tsx
+++ instagram-app/src/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./store";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./store";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
@@ -12,11 +13,13 @@ const root = ReactDOM.createRoot(
 );
 root.render(
     <Provider store={store}>
-        <React.StrictMode>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </React.StrictMode>
+        <PersistGate loading={null} persistor={persistor}>
+            <React.StrictMode>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </React.StrictMode>
+        </PersistGate>
     </Provider>
 );
 reportWebVitals();
