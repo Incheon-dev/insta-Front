c879bc7234fb528c5d2750d37fc0a2e86aa0ebd0
dongeun-l <dongeun1014@naver.com>
2023-02-11 20:03:08 +0900

    select comp 추가

---
+++ instagram-app/src/components/select/index.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+export type SelectPropsType = {
+    value?: string;
+    onChange: (v: any) => void;
+    radius?: number | string;
+    options: OptionPropsType[];
+};
+
+export type OptionPropsType = {
+    text: string;
+    value?: string | number;
+};
+
+export const Select: React.FunctionComponent<SelectPropsType> = (props) => {
+    return (
+        <DefaultSelect
+            value={props.value}
+            onChange={(e: any) => {
+                props.onChange(e);
+            }}
+            radius={props.radius}
+        >
+            {props.options.map((option: OptionPropsType) => {
+                return (
+                    <Option value={option.value ? option.value : option.text}>
+                        {option.text}
+                    </Option>
+                );
+            })}
+        </DefaultSelect>
+    );
+};
+
+type StyledProps = {
+    radius?: number | string;
+    width?: number;
+};
+
+const DefaultSelect = styled.select`
+    padding: 10px 15px;
+    border: 1px solid #ccc;
+    width: 100%;
+    background-color: #fff;
+    box-sizing: border-box;
+    border-radius: ${(props: StyledProps) =>
+        typeof props.radius == "string" ? props.radius : `${props.radius}px`};
+`;
+
+const Option = styled.option``;
