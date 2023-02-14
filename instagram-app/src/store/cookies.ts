9b08b65f937177c9546535c90b7c49d6e9206c06
dongeun-l <dongeun1014@naver.com>
2023-02-14 18:07:28 +0900

    add cookies

---
+++ instagram-app/src/store/cookies.ts
@@ -0,0 +1,19 @@
+import { Cookies } from "react-cookie";
+
+const cookies = new Cookies();
+
+export const setCookie = (name: string, value: string, option?: any) => {
+    return cookies.set(name, value, { ...option });
+};
+
+export const getCookie = (name: string) => {
+    return cookies.get(name);
+};
+
+export const getAllCookie = (option?: any) => {
+    return cookies.getAll({ ...option });
+};
+
+export const removeCookie = (name: string, option?: any) => {
+    return cookies.remove(name, { ...option });
+};
