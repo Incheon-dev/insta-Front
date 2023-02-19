bc30cb16ff916da4d77c0e5830829f384c292ba7
dongeun-l <dongeun1014@naver.com>
2023-02-19 15:04:35 +0900

    remove drawer

--- instagram-app/src/pages/home.page.tsx
+++ instagram-app/src/pages/home.page.tsx
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import ResponsiveDrawer from "../components/navigation";
 import * as S from "@/styled";
 
 const HomePage = () => {
-    return <div>홈페이지</div>;
+    return (
+        <>
+            <div>ddfsfs</div>
+        </>
+    );
 };
 export default HomePage;
