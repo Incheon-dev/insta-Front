c582f6798b34c8bf57f0a70fac3512f12c3b7b71
dongeun-l <dongeun1014@naver.com>
2023-02-19 15:55:16 +0900

    import fix

--- instagram-app/src/pages/home.page.tsx
+++ instagram-app/src/pages/home.page.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import ResponsiveDrawer from "../components/navigation";
 import * as S from "@/styled";
 
 const HomePage = () => {
