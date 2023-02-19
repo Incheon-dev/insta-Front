f2a28fd2e30a2ce29f44c2e674b3de6daf4cc583
dongeun-l <dongeun1014@naver.com>
2023-02-19 15:54:57 +0900

    add laylout

--- instagram-app/src/App.tsx
+++ instagram-app/src/App.tsx
@@ -1,36 +1,22 @@
 import React, { useState, useEffect, Suspense } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+import { Layout } from "./layout";
 import "./App.css";
-import { Modal } from "./components/";
+
 import { PageList } from "./pages/page_list";
-import ResponsiveDrawer from "./components/navigation";
-import { useLocations } from "./hooks";
-import { navMenus } from "./constant/menus";
 const App: React.FunctionComponent<any> = () => {
-    const { path } = useLocations();
     const [isLogin, setisLogin] = useState<boolean>(false);
     const [useDrawer, setUseDrawer] = useState<boolean>(false);
     useEffect(() => {
         let _isLogin = window.sessionStorage.getItem("isLogin");
         if (_isLogin) setisLogin(true);
     }, []);
-    useEffect(() => {
-        /* 로그인  , 회원가입페이지에서는 노출x */
-        if (path?.indexOf("/login") != -1 && path?.indexOf("/signup") != -1) {
-            setUseDrawer(true);
-        } else {
-            setUseDrawer(false);
-        }
-    }, [path]);
 
     return (
         <Suspense fallback={<div>Loading...</div>}>
-            <Modal />
-            {/* 로그인  , 회원가입페이지에서는 노출xc */}
-            {useDrawer && (
-                <ResponsiveDrawer navList={navMenus} visible={useDrawer} />
-            )}
-            <PageList isLogin={isLogin} />
+            <Layout>
+                <PageList isLogin={isLogin} />
+            </Layout>
         </Suspense>
     );
 };
---
+++ instagram-app/src/layout/index.tsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect, ReactElement } from "react";
+import ResponsiveDrawer from "../components/navigation";
+import { navMenus } from "../constant/menus";
+import { useLocations } from "../hooks";
+import { Modal } from "../components/";
+import styled from "styled-components";
+
+export type layoutProps = {
+    children?: React.ReactNode;
+};
+
+export const Layout: React.FunctionComponent<layoutProps> = (props) => {
+    const { path } = useLocations();
+    const [useDrawer, setUseDrawer] = useState<boolean>(false);
+    useEffect(() => {
+        /* 로그인  , 회원가입페이지에서는 노출x */
+        if (path?.indexOf("/login") != -1 || path?.indexOf("/signup") != -1) {
+            setUseDrawer(false);
+        } else {
+            setUseDrawer(true);
+        }
+    }, [path]);
+    return (
+        <Container>
+            <Modal />
+            {useDrawer && <ResponsiveDrawer navList={navMenus} />}
+            <Main>{props.children}</Main>
+        </Container>
+    );
+};
+const Container = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+`;
+const Main = styled.main`
+    box-sizing: border-box;
+    width: 100%;
+`;
